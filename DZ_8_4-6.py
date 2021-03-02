fd450f830cf881a4932d079203a72858f088b0f6
SergeyTaranishin <tat12.12.1982@mail.ru>
2021-03-02 20:30:25 +0400

    Ура! работает! не совсем красиво конечно, но работает. Я очень рад ))))

--- DZ_8_4-6.py
+++ DZ_8_4-6.py
@@ -5,44 +5,67 @@ class Warehouse:
         self.square = square
 
 class Office_equipment:
-    count = 0
+    count_all = 0
+    count_office = 0
     list = []
     def __init__(self, type, model, price):
         self.type = type
         self.model = model
         self.price = price
         self.place = 'склад'
-        Office_equipment.count += 1
+        Office_equipment.count_all += 1
         Warehouse.count += 1
 
-    def Move_to_Warehouse(self):
+    def Move_to_Warehouse(self, ind):
         if self.place == 'склад':
             print(f'{self.type} {self.model} уже на складе')
         else:
             self.place = 'склад'
+            Office_equipment.list[ind - 1].update({'Нахождение':'склад'})
             Warehouse.count += 1
-        # действие осталось неотлаженым, так как не получилось на него передать объект
+            Office_equipment.count_office -= 1
+
+    def Move_to_Office(self, ind):
+        if self.place == 'склад':
+            self.place = 'офис'
+            Office_equipment.list[ind - 1].update({'Нахождение':'офис'})
+            Warehouse.count -= 1
+            Office_equipment.count_office += 1
+        else:
+            print(f'{self.type} {self.model} уже в офисе')
 
 class Printer(Office_equipment):
     def __init__(self, type, model, price, color):
         Office_equipment.__init__(self, type, model, price)
         self.color = color
-        self.id = self
-        Office_equipment.list.append(self.__dict__)
+        Office_equipment.list.append({'Тип оборудования': self.type,
+                                      'Модель': self.model,
+                                      'Цена': self.price,
+                                      'Цветность': self.color,
+                                      'Нахождение': self.place,
+                                      'ID': self})
 
 class Scanner(Office_equipment):
     def __init__(self, type, model, price, format):
         Office_equipment.__init__(self, type, model, price)
         self.format = format
-        self.id = self
-        Office_equipment.list.append(self.__dict__)
+        Office_equipment.list.append({'Тип оборудования': self.type,
+                                      'Модель': self.model,
+                                      'Цена': self.price,
+                                      'Формат': self.format,
+                                      'Нахождение': self.place,
+                                      'ID': self})
 
 class Xerox(Office_equipment):
     def __init__(self, type, model, price, speed):
         Office_equipment.__init__(self, type, model, price)
         self.speed = speed
-        self.ID = self
-        Office_equipment.list.append(self.__dict__)
+        Office_equipment.list.append({'Тип оборудования': self.type,
+                                      'Модель': self.model,
+                                      'Цена': self.price,
+                                      'Скорость': self.speed,
+                                      'Нахождение': self.place,
+                                      'ID': self})
 
 obj_warehouse = Warehouse(1500)
 obj_printer_1 = Printer('Принтер', 'HP107', 130, 'b/w')
@@ -52,32 +75,29 @@ obj_scanner_2 = Scanner('Сканер', 'CanoScan Lide 92', 110, 'A4')
 obj_xerox_1 = Xerox('Ксерокс', 'CANON imageRUNNER C1225iF', 890, 25)
 obj_xerox_2 = Xerox('Ксерокс', 'CANON imageRUNNER C1485iF', 1090, 35)
 
-# попутки понять как вытащить ID объекта
-# print(dir(0x0000023DA2CE6D00))
-# print(str(obj_printer_1.id)[str(obj_printer_1.id).index(' at ')+4:-1])
-
 while True:
-    print(f'На складе: {Warehouse.count}          Всего:{Office_equipment.count}')
+    print(f'На складе: {Warehouse.count}          В офисе: {Office_equipment.count_office}          Всего:{Office_equipment.count_all}')
     for i, v in enumerate(Office_equipment.list):
         print(f'{i + 1}: {v}')
-    print('Выберите оборудование для перемещения (цифра). 0 - завершение работы: ', end='')
+    print(f'Выберите оборудование для перемещения (число от 1 до {len(Office_equipment.list)}). 0 - завершение работы: ', end='')
     try:
-        x = int(input())
-        if x == 0:
+        ind = int(input())
+
+        if ind == 0:
             break
-        print('Куда переместить? 1 - Склад, 2 - Организация: ', end='')
-        try:
-            where = int(input())
-            if where == 1:
-                # Тут я не успел найти решение. Мысль заключалась в том, что бы помещать объекты в справочник
-                # со словарями, и потом их извлекать по указаному индексу X. Но как правильно помещать объект
-                # в справочник/список и извлекать его я нашёл. (((
-                # Программа осталась недоделаной. А были мысли дополнить атрибутом "количество оборудования" и при
-                # переносе указывать количество... но не судьба (((
-                pass
-                # Move_to_Warehouse(str(Office_equipment.list[x-1].get('id'))[str(Office_equipment.list[x-1].get('id')).index(' at ')+4:-1])
-        except ValueError:
-            print('Нужно ввести число!')
+        elif 0 < ind <= len(Office_equipment.list):
+            print('Куда переместить? 1 - Склад, 2 - Офис: ', end='')
+            try:
+                where = int(input())
+                if where == 1:
+                    Office_equipment.Move_to_Warehouse(Office_equipment.list[ind-1].get('ID'), ind)
+                elif where == 2:
+                    Office_equipment.Move_to_Office(Office_equipment.list[ind-1].get('ID'), ind)
+                else:
+                    print('Нужно ввести 1 или 2')
+            except ValueError:
+                print('Нужно ввести число!')
+        else:
+            print(f'Нужно ввести число от 1 до {len(Office_equipment.list)}')
     except ValueError:
         print('Нужно ввести число!')
-
